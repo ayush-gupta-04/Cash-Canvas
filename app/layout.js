731dbd8a7793645827a190d8d190732bf34e52c6
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2025-11-23 12:26:14 +0530

    Update footer text

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
 
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>Developed by Sunny Kumar</p>
+              <p>All rights are reserved</p>
             </div>
           </footer>
         </body>
